c1e40eca99189bdcddbad350e5bbd3c79faef7c1
Infarh <shmachilin@yandex.ru>
2021-12-03 19:34:35 +0300

    Обеспечение связи с таблицами БД

--- ConsoleEF/Context/StudentsDB.cs
+++ ConsoleEF/Context/StudentsDB.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ConsoleEF.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace ConsoleEF.Context
 {
     public class StudentsDB : DbContext
     {
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Group> Groups { get; set; }
+
         public StudentsDB(DbContextOptions<StudentsDB> opt) : base(opt)
         {
             
