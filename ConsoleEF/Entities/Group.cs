c18c298f089d40a0a7f5fbd1a2cea9614d302bc2
Infarh <shmachilin@yandex.ru>
2021-12-03 19:27:10 +0300

    Определяем структуру БД - сущности

---
+++ ConsoleEF/Entities/Group.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleEF.Entities
+{
+    public class Group
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        // Устанавливаем связь между таблицами 1-ко-многим
+        public ICollection<Student> Students { get; set; } = new HashSet<Student>();
+    }
+}
---
+++ ConsoleEF/Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleEF.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
