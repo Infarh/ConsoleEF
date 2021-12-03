6aed2abf5ef865e8dfadeab4ff3a8a9db29d2895
Infarh <shmachilin@yandex.ru>
2021-12-03 19:29:41 +0300

    Добавление класса-контекста БД

---
+++ ConsoleEF/Context/StudentsDB.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleEF.Context
+{
+    public class StudentsDB : DbContext
+    {
+        public StudentsDB(DbContextOptions<StudentsDB> opt) : base(opt)
+        {
+            
+        }
+    }
+}
